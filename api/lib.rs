60bb3f127ec82e617eab7734e044dcbd72a259dc
Andezion <vdoronchenkov@gmail.com>
2026-05-18 13:55:21 +0200

    add: basic structure

---
+++ api/lib.rs
@@ -0,0 +1,26 @@
+
+
+pub struct LeetcodeApi {
+
+}
+
+pub struct CodeforceApi {
+
+}
+
+pub struct CodewarsApi {
+
+}
+
+pub struct GithubLangsApi {
+
+}
+
+pub struct GithubVisitorsApi {
+
+}
+
+pub struct GithubStreakApi {
+    
+}
+    
