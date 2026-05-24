b56987bee403834d72d7bb5d08ec1488fd76ceeb
Andezion <vdoronchenkov@gmail.com>
2026-05-24 21:25:27 +0200

    add: new stuff

--- api/leetcode.rs
+++ api/leetcode.rs
@@ -1,7 +1,19 @@
+struct Badge {
+    id: u32,
+    name: String,
+    icon_url: String,
+    date: String
+}
 
+struct BadgeStorage {
+    counter: u32, 
+    badges: Vec<Badge>
+}
 
 impl LeetcodeApi {
     pub fn amount_of_solved_problems(&self, username: &str) -> Option<u32> {
         
     }
+
+    pub fn badges(&self, username: &str) -> Option<Vec<BadgeStorage>> {}
 }
