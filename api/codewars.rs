fc739c51d1c372728958e4606466a7d1a58e0cf9
Andezion <vdoronchenkov@gmail.com>
2026-05-24 23:39:04 +0200

    add: started codewars

--- api/codewars.rs
+++ api/codewars.rs
@@ -3,6 +3,11 @@ use std::time::Duration;
 
 const DEFAULT_BASE_URL: &str = "https://www.codewars.com/api/v1/users/";
 
+pub struct CodeChallenge {
+    pub total_authored: u32, 
+    pub total_completed: u32,
+}
+
 impl CodewarsApi {
     pub fn new(base_url: impl Into<String>) -> Self {
         CodewarsApi {
@@ -12,5 +17,9 @@ impl CodewarsApi {
                 .build()
                 .expect("failed to build HTTP client"),
         }
+    }
+
+    pub fn amount_of_completed_challenges() {
+
     }
 }
