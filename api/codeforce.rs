596eb6c2f562b2a2aa38c13e9d79efbfaf33536c
Andezion <vdoronchenkov@gmail.com>
2026-05-25 15:38:52 +0200

    add: checked codeforce

--- api/codeforce.rs
+++ api/codeforce.rs
@@ -5,6 +5,42 @@ const DEFAULT_BASE_URL: &str = "https://codeforces.com/api";
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+pub enum Verdict {
+    FAILED,
+    OK,
+    PARTIAL,
+    COMPILATION_ERROR,
+    RUNTIME_ERROR,
+    WRONG_ANSWER,
+    TIME_LIMIT_EXCEEDED,
+    MEMORY_LIMIT_EXCEEDED,
+    IDLENESS_LIMIT_EXCEEDED,
+    SECURITY_VIOLATED,
+    CRASHED,
+    INPUT_PREPARATION_CRASHED,
+    CHALLENGED,
+    SKIPPED,
+    TESTING,
+    REJECTED,
+    SUBMITTED,
+}
+
+pub struct Submission {
+    pub id: i32, 	
+    contestId 	Integer. Can be absent.
+    creationTimeSeconds 	Integer. Time, when submission was created, in unix-format.
+    relativeTimeSeconds 	Integer. Number of seconds, passed after the start of the contest (or a virtual start for virtual parties), before the submission.
+    problem 	Problem object.
+    author 	Party object.
+    programmingLanguage 	String.
+    verdict 	Enum: FAILED, OK, PARTIAL, COMPILATION_ERROR, RUNTIME_ERROR, WRONG_ANSWER, WRONG_ANSWER, TIME_LIMIT_EXCEEDED, MEMORY_LIMIT_EXCEEDED, IDLENESS_LIMIT_EXCEEDED, SECURITY_VIOLATED, CRASHED, INPUT_PREPARATION_CRASHED, CHALLENGED, SKIPPED, TESTING, REJECTED, SUBMITTED. Can be absent.
+    testset 	Enum: SAMPLES, PRETESTS, TESTS, CHALLENGES, TESTS1, ..., TESTS10. Testset used for judging the submission.
+    passedTestCount 	Integer. Number of passed tests.
+    timeConsumedMillis 	Integer. Maximum time in milliseconds, consumed by solution for one test.
+    memoryConsumedBytes 	Integer. Maximum memory in bytes, consumed by solution for one test.
+    points 	Floating point number. Can be absent. Number of scored points for IOI-like contests.
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct User {
