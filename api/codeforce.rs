5de30a9729ec5eac826afd017b6d02718ab089e2
Andezion <vdoronchenkov@gmail.com>
2026-05-25 15:39:06 +0200

    add: checked codeforce

--- api/codeforce.rs
+++ api/codeforce.rs
@@ -25,6 +25,23 @@ pub enum Verdict {
     SUBMITTED,
 }
 
+pub enum Testset {
+    SAMPLES,
+    PRETESTS,
+    TESTS,
+    CHALLENGES,
+    TESTS1,
+    TESTS2,
+    TESTS3,
+    TESTS4,
+    TESTS5,
+    TESTS6,
+    TESTS7,
+    TESTS8,
+    TESTS9,
+    TESTS10,
+}
+
 pub struct Submission {
     pub id: i32, 	
     contestId 	Integer. Can be absent.
