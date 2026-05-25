a426f1350a2e512701e3284560db6fb2b0eef6ff
Andezion <vdoronchenkov@gmail.com>
2026-05-25 15:03:11 +0200

    add: working on codeforce api

--- api/codeforce.rs
+++ api/codeforce.rs
@@ -9,7 +9,21 @@ pub struct User {
     pub vkid: String,
     #[serde(rename = "openId")]
     pub openid: String,
-
+    pub firstName: String,
+    pub lastName: String,   
+    pub country: String,
+    pub city: String,
+    pub organization: String,
+    pub contribution: i32,
+    pub rank: String,
+    pub rating: i32,
+    pub maxRank: String,
+    pub maxRating: i32,
+    pub lastOnlineTime: i64,
+    pub registrationTime: i64,
+    pub friendOf: i64,
+    pub avatar: String,
+    pub titlePhoto: String,
 }
 
 impl CodeforcesApi {
