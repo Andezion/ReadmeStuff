c4466293087d74510cf8ca2478a399cc93c90e80
Andezion <vdoronchenkov@gmail.com>
2026-05-25 15:01:06 +0200

    add: started codeforce api

--- api/codeforce.rs
+++ api/codeforce.rs
@@ -2,6 +2,14 @@ use serde::Deserialize;
 
 const DEFAULT_BASE_URL: &str = "https://www.codewars.com/api/";
 
+pub struct User {
+    pub handle: String,
+    pub email: String,
+    pub vkid: String,
+    pub openId: String,
+
+}
+
 impl CodeforcesApi {
     
 }
