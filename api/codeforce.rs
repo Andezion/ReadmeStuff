fbf311ee45275a2a837622750f093421b553df42
Andezion <vdoronchenkov@gmail.com>
2026-05-25 15:01:42 +0200

    add: working on codeforce api

--- api/codeforce.rs
+++ api/codeforce.rs
@@ -2,11 +2,13 @@ use serde::Deserialize;
 
 const DEFAULT_BASE_URL: &str = "https://www.codewars.com/api/";
 
+#[derive(Deserialize)]
 pub struct User {
     pub handle: String,
     pub email: String,
     pub vkid: String,
-    pub openId: String,
+    #[serde(rename = "openId")]
+    pub openid: String,
 
 }
 
