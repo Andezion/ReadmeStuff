ab5b40b3f21a2b080b2766f9fcbf33d2eda39b2a
Andezion <vdoronchenkov@gmail.com>
2026-06-06 22:28:20 +0200

    add: added error stuff

--- aggregator/errors.rs
+++ aggregator/errors.rs
@@ -1,1 +1,13 @@
-// типа клин код и ошибки в другом месте 
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AggregatorError {
+    #[error("github: {0}")]
+    GitHub(String),
+    #[error("codeforces: {0}")]
+    Codeforces(String),
+    #[error("codewars: {0}")]
+    Codewars(String),
+    #[error("leetcode: {0}")]
+    LeetCode(String),
+}
