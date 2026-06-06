a3a1e37cf0867be9637faf97b9c0d003e2334a48
Andezion <vdoronchenkov@gmail.com>
2026-06-06 22:25:36 +0200

    add: added new stuff to models

--- aggregator/models.rs
+++ aggregator/models.rs
@@ -1,21 +1,38 @@
-// тут будут структуры валятся наши
+use readme_stuff_api::{
+    github_statistic::ProfileStats,
+    github_streak::StreakStats,
+    github_langs::AggregatedLangStats,
+    codeforce::{User as CfUser, RatingChange},
+    codewars::User as CwUser,
+    leetcode::{Solved, Language as LcLanguage},
+    github_visitors::models::VisitorAnalytics,
+};
 
-struct UserProfile {
-
+pub struct CfData {
+    pub user: CfUser,
+    pub rating_history: Vec<RatingChange>,
 }
 
-struct ProfileSection {
-
+pub struct LcData {
+    pub solved: Solved,
+    pub languages: Vec<LcLanguage>,
 }
 
-struct WidgetData {
-
+pub struct UserProfile {
+    pub github:     Option<ProfileStats>,
+    pub streak:     Option<StreakStats>,
+    pub langs:      Option<AggregatedLangStats>,
+    pub codeforces: Option<CfData>,
+    pub codewars:   Option<CwUser>,
+    pub leetcode:   Option<LcData>,
+    pub visitors:   Option<VisitorAnalytics>,
+    pub sources:    SourceStatus,
 }
 
-struct DashboardData {
-
-}
-
-struct SourceStatus {
-    
+pub struct SourceStatus {
+    pub github:     Result<(), String>,
+    pub codeforces: Result<(), String>,
+    pub codewars:   Result<(), String>,
+    pub leetcode:   Result<(), String>,
+    pub visitors:   Result<(), String>,
 }
