268cbacf56c269696be67f901744cc45c2ac3e40
Andezion <vdoronchenkov@gmail.com>
2026-07-02 14:55:37 +0200

    fix: other fixes

--- draw/src/github_visitors.rs
+++ draw/src/github_visitors.rs
@@ -6,7 +6,7 @@ use crate::{
 use readme_stuff_aggregator::widgets::GithubVisitorsWidget;
 
 const W: u32 = 400;
-const H: u32 = 360;
+const H: u32 = 320;
 
 pub fn render_github_visitors(w: &GithubVisitorsWidget, theme: Theme) -> String {
     let c = theme.colors();
