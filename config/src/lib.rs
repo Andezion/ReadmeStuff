d159b70c59b5ece599310aac93c908f535bc7d40
Andezion <vdoronchenkov@gmail.com>
2026-07-21 20:15:53 +0200

    add: started doing config

---
+++ config/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod credential;
+pub mod defaults;
+pub mod io;
+pub mod schema;
+
+pub use credential::{Credential, Requirement};
+pub use schema::{Config, Layout, PlacedWidget, ProfileConfig, Row, ThemeChoice};
