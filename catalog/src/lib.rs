aea3b1a4726f09cdf533a16b5aa48708945ba370
Andezion <vdoronchenkov@gmail.com>
2026-07-25 11:52:45 +0200

    add: added new stuff to lib

--- catalog/src/lib.rs
+++ catalog/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod pipeline;
 pub mod registry;
 
-pub use pipeline::{BuildOutput, WidgetOutcome, build};
+pub use pipeline::{BuildOutput, TextCardOutcome, WidgetOutcome, build};
 pub use registry::{WidgetGroup, WidgetSpec, all_widgets, find};
