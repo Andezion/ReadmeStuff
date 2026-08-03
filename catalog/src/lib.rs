d92d70f3f2084f052fbf6f4d556c43b4b5beeab6
Andezion <vdoronchenkov@gmail.com>
2026-08-03 15:37:54 +0200

    add: added new path stuff

--- catalog/src/lib.rs
+++ catalog/src/lib.rs
@@ -1,5 +1,9 @@
+pub mod discovery;
 pub mod pipeline;
+pub mod placement;
 pub mod registry;
 
+pub use discovery::{DiscoveredWidget, TEXT_WIDGETS_DIR, WIDGETS_DIR, scan_widgets};
 pub use pipeline::{BuildOutput, TextCardOutcome, WidgetOutcome, build};
+pub use placement::{Rect, SNAP_TOLERANCE, fits_canvas, overlaps, snap};
 pub use registry::{WidgetGroup, WidgetSpec, all_widgets, find};
